00ac4cedfa5b6a6109d88c8e3116d23e81f1e5c7
JamieGainer <jgainer137@gmail.con>
2019-11-25 17:10:33 -0800

    Work on slicing

--- einstein_tensor/einstein_tensor.py
+++ einstein_tensor/einstein_tensor.py
@@ -1,98 +1,171 @@
+from __future__ import annotations
 import numpy as np
 
-class Tensor():
 
-    def __init__(self, value, indices):
+class Tensor():
+    """
+    Creates a tensor with indices.  Multiplication of these tensors automatically follows the Einstein summation
+    convention including contracting a raised index in the first tensor with a lowered index in the second
+    tensor.
+
+    Initialized with
+    Args:
+        value: a numpy array, list, or tuple containing the values in the tensor
+        indices: a list or numpy array of strings describing the indices.
+
+    These arguments are cast into numpy arrays which are the public attributes of the class.
+    ddd"""
+
+    def __init__(self, value, indices) -> None:
         self.value = np.array(value)
         self.indices = np.array(indices)
 
-    def __eq__(self, other):
-        return np.all(self.value == other.value) and np.all(self.indices == other.indices)
+    def __eq__(self, other) -> bool:
+        try:
+            equal_values = np.array_equal(self.value, other.value)
+            equal_indices = np.array_equal(self.indices, other.indices)
+            return equal_values and equal_indices
+        except:
+            if self.value.shape == ():
+                return self.value == other
+            return False
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.value) + " " + str(self.indices)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
-    def __add__(self, other):
+    def __add__(self, other: Tensor) -> Tensor:
         if self.indices != other.indices:
             raise ValueError("Tensors have different indices so cannot be added.")
         return Tensor(self.value + other.value, self.indices)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Tensor) -> Tensor:
         return self + -other
 
-    def __neg__(self):
+    def __neg__(self) -> Tensor:
         return Tensor(-self.value, self.indices)
 
-    def __rmul__(self, other):
-        return self.right_multiply_scalar(other)
+    def __rmul__(self, other) -> Tensor:
+        return self._left_multiply_tensor_by_scalar(other)
 
-    def __mul__(self, other):
+    def __mul__(self, other) -> Tensor:
         try:
-            if self.there_are_repeated_indices(other):
-                raise ValueError("Cannot multiply tensors with identical indices.")
-            return self.multiply_tensors(other)
+            return self._multiply_by_tensor(other)
         except AttributeError:
-            return self.left_multiply_scalar(other)
+            return self._right_multiply_tensor_by_scalar(other)
 
-    def there_are_repeated_indices(self, other) -> bool:
-        my_indices = set(self.indices)
-        other_indices = set(other.indices)
-        return len(my_indices.union(other_indices)) < len(my_indices) + len(other.indices)
+    def __getitem__(self, tuple_of_indices):
+        value_error_string = "Indices for tensor must be an integer or an empty slice for each index."
+        if type(tuple_of_indices) is not tuple:
+            tuple_of_indices = (tuple_of_indices, )
+        if len(tuple_of_indices) != len(self.indices):
+            raise ValueError(value_error_string)
+        list_of_indices = []
+        for i_index, index in enumerate(tuple_of_indices):
+            if type(index) is int:
+                pass
+            elif type(index) is slice:
+                if slice.start is None and slice.stop is None and slice.step is None:
+                    list_of_indices.append(self.indices[i_index])
+                else:
+                    raise ValueError(value_error_string)
+            else:
+                raise ValueError(value_error_string)
+        return Tensor(self.value[tuple_of_indices], list_of_indices)
 
-    def multiply_tensors(self, other):
-        self.initialize_data_structures_for_multiplication(other)
-        self.fill_in_data_structures_for_multiplication()
-        new_value, new_indices = self.obtain_new_value_and_indices(other)
-        self.remove_data_structures()
-        return Tensor(new_value, new_indices)
 
-    def left_multiply_scalar(self, other):
+    def _multiply_by_tensor(self, other: Tensor) -> Tensor:
+        # create a _TensorMultiplier object and use it to perform the multiplication
+        tensor_multiplier = _TensorMultiplier(self, other)
+        return tensor_multiplier._result()
+
+    def _right_multiply_tensor_by_scalar(self, other) -> Tensor:
+        # perform tensor * scalar multiplication
         new_value = self.value * other
         new_indices = self.indices
         return Tensor(new_value, new_indices)
 
-    def initialize_data_structures_for_multiplication(self, other):
-        self.my_contract_indices, self.other_contract_indices = [], []
-        self.my_indices_to_remove, self.other_indices_to_remove = set([]), set([])
-        self.other_indices_dict = {index: i_index for i_index, index in enumerate(other.indices)}
-
-    def fill_in_data_structures_for_multiplication(self):
-        for i_index, index in enumerate(self.indices):
-            search_index = self.complimentary_index(index)
-            if search_index in self.other_indices_dict:
-                self.update_contracted_index_lists(i_index, index, search_index)
-
-    def obtain_new_value_and_indices(self, other):
-        new_value = np.tensordot(self.value, other.value, axes=(self.my_contract_indices, self.other_contract_indices))
-        new_indices = [index for index in self.indices if index not in self.my_indices_to_remove]
-        new_indices += [index for index in other.indices if index not in self.other_indices_to_remove]
-        return new_value, new_indices
-
-    def remove_data_structures(self):
-        del self.my_contract_indices
-        del self.other_contract_indices
-        del self.my_indices_to_remove
-        del self.other_indices_to_remove
-        del self.other_indices_dict
-
-    def complimentary_index(self, index: str) -> str:
-        compliment_dict = {'^': '_', '_': '^'}
-        for index_start in compliment_dict:
-            if index.startswith(index_start):
-                return compliment_dict[index_start] + index[len(index_start):]
-        raise ValueError("Index must be either raised (with initial '^') or lowered " +
-                             "(with initial ('_').")
-
-    def update_contracted_index_lists(self, i_index: int, index: str, search_index: str):
-        self.my_contract_indices.append(i_index)
-        self.other_contract_indices.append(self.other_indices_dict[search_index])
-        self.my_indices_to_remove.add(index)
-        self.other_indices_to_remove.add(search_index)
-
-    def right_multiply_scalar(self, other):
+    def _left_multiply_tensor_by_scalar(self, other) -> Tensor:
+        # perform scalar * tensor multiplication
         new_value = other * self.value
         new_indices = self.indices
         return Tensor(new_value, new_indices)
+
+
+class _TensorMultiplier():
+    # Internal class to perform Einstein summation convention tensor multiplications with (possibly)
+    # raised or lowered indices
+
+    def __init__(self, tensor_a: Tensor, tensor_b: Tensor) -> None:
+        # class takes two instantiations of Tensor as its arguments
+        self._a, self._b = tensor_a, tensor_b
+        self._a_contract_indices, self._b_contract_indices, self._a_keep_indices= [], [], []
+        self._b_remove_indices = set([])
+        self._b_indices_index = {index: i_index for i_index, index in enumerate(tensor_b.indices)}
+
+    def _result(self) -> Tensor:
+        # returns the Tensor found by multiplying self._a and self._b
+        self._process_indices()
+        new_value = np.tensordot(self._a.value, self._b.value,
+                                 axes=(self._a_contract_indices, self._b_contract_indices))
+        new_indices = self._a_keep_indices + self._b_keep_indices
+        return Tensor(new_value, new_indices)
+
+    def _process_indices(self) -> None:
+        # prepares for the multiplication by making lists describing the indices to keep and
+        # those to sum over
+        for i_index, index in enumerate(self._a.indices):
+            self._check_for_repeated_raised_or_lowered_index(index)
+            complimentary_index = self._obtain_complimentary_method(index)
+            self._update_index_structures(i_index, index, complimentary_index)
+        self._b_keep_indices = [index for index in self._b.indices if index not in self._b_remove_indices]
+
+    def _check_for_repeated_raised_or_lowered_index(self, index: str) -> None:
+        # return a ValueError if a raised or lowered index occurs in both self._a and self._b
+        if not self._is_raised_or_lowered(index):
+            return
+        if index in self._b_indices_index:
+            raise ValueError("Cannot multiply tensors with identical raised or lowered indices.")
+
+    def _is_raised_or_lowered(self, index: str) -> bool:
+        # returns true if coordinate corresponds to a raised ('^') or lowered ('_') index, false otherwise
+        return index[0] in ['^', '_']
+
+    def _obtain_complimentary_method(self, index: str) -> str:
+        # return the string describing a lowered index, if index is a raised index string (and vice versa)
+        # if the input string in "index" is neither raised or lowered, return index
+        complimentary_index = index
+        if self._is_raised_or_lowered(index):
+            complimentary_index = self._complimentary_index(index)
+        return complimentary_index
+
+    def _complimentary_index(self, index: str) -> str:
+        # return the string describing a lowered index, if index is a raised index string (and vice versa)
+        if index[0] == '^':
+            return '_' + index[1:]
+        elif index[0] == '_':
+            return '^' + index[1:]
+        return index
+
+    def _update_index_structures(self, i_index: int, index: str, complimentary_index) -> None:
+        # fills in the appropriate index structures depending on whether or not
+        # complimentary_index is one of self._b's indices
+        if complimentary_index in self._b_indices_index:
+            self._index_in_a_compliment_in_b(i_index, complimentary_index)
+        else:
+            self._index_not_in_b(index)
+
+    def _index_in_a_compliment_in_b(self, i_index: int, complimentary_index: str) -> None:
+        # fills in the appropriate index structures when complimentary_index is one
+        # of self._b's indices
+        self._a_contract_indices.append(i_index)
+        self._b_contract_indices.append(self._b_indices_index[complimentary_index])
+        self._b_remove_indices.add(complimentary_index)
+
+    def _index_not_in_b(self, index: str) -> None:
+        # fills int the appropriate index structure when complimentary_index is not
+        # one of self._b's indices
+        self._a_keep_indices.append(index)
+
--- einstein_tensor/tests/test.py
+++ einstein_tensor/tests/test.py
@@ -12,61 +12,88 @@ class TestEinsteinTensor(unittest.TestCase):
     assert a_indices[0][0] == '^'
     a = et.Tensor(a_value, a_indices)
 
-    def test_identical_tensors_are_equal(self):
+    def test_identical_tensors_are_equal(self) -> None:
         b = et.Tensor(self.a_value, self.a_indices)
         self.assertEqual(self.a, b)
 
-    def test_tensors_with_different_shapes_are_not_equal(self):
+    def test_tensors_with_different_shapes_are_not_equal(self) -> None:
         b = et.Tensor(self.a_value * 2, self.a_indices)
         self.assertNotEqual(self.a, b)
 
-    def test_tensors_with_different_values_are_not_equal(self):
+    def test_tensors_with_different_values_are_not_equal(self) -> None:
         b = et.Tensor(2 * np.array(self.a_value), self.a_indices)
         self.assertNotEqual(self.a, b)
 
-    def test_tensors_with_different_indices_are_not_equal(self):
+    def test_tensors_with_different_indices_are_not_equal(self) -> None:
         b = et.Tensor(self.a_value, [self.a_indices[0] + '*'])
         self.assertNotEqual(self.a, b)
 
-    def test_adding_two_tensors(self):
+    def test_adding_two_tensors(self) -> None:
         b = et.Tensor(2 * np.array(self.a_value), self.a_indices)
         self.assertEqual(self.a + self.a, b)
 
-    def test_exception_raised_when_adding_tensors_with_different_indices(self):
+    def test_exception_raised_when_adding_tensors_with_different_indices(self) -> None:
         b = et.Tensor(self.a_value, [self.a_indices[0] + '*'])
         with self.assertRaises(ValueError):
             c = self.a + b
 
-    def test_cannot_multiply_tensors_with_the_same_index(self):
+    def test_cannot_multiply_tensors_with_the_same_index(self) -> None:
         b = et.Tensor(self.a_value, self.a_indices)
         with self.assertRaises(ValueError):
             c = self.a * b
 
-    def test_multiply_vector_and_1_form(self):
+    def test_multiply_vector_and_1_form(self) -> None:
         lowered_indices = [self.a_indices[0].replace('^', '_')]
         b = et.Tensor(self.a_value, lowered_indices)
         vector_squared_value = self.a_value[0]**2 - sum([x**2 for x in self.a_value[1:]])
         self.assertEqual((self.a * b).value, vector_squared_value)
 
-    def test_right_multiply_vector_by_constant(self):
+    def test_right_multiply_vector_by_constant(self) -> None:
         self.assertEqual(self.a * 1, self.a)
 
-    def test_left_multiply_vector_by_constant(self):
+    def test_left_multiply_vector_by_constant(self) -> None:
         self.assertEqual(1 * self.a, self.a)
 
-    def test_str_tensor(self):
+    def test_str_tensor(self) -> None:
         self.assertEqual(self.a.__str__(), str(self.a.value) + " " + str(self.a.indices))
 
-    def test_repr_tensor(self):
+    def test_repr_tensor(self) -> None:
         self.assertEqual(self.a.__repr__(), self.a.__str__())
 
-    def test_neg_tensor_plus_tensor_equals_zero_tensor(self):
+    def test_neg_tensor_plus_tensor_equals_zero_tensor(self) -> None:
         zero_list = [0 for x in self.a_value]
         self.assertEqual(self.a + -self.a, et.Tensor(zero_list, self.a_indices))
 
-    def test_tensor_minus_itself_equals_zero_tensor(self):
+    def test_tensor_minus_itself_equals_zero_tensor(self) -> None:
         zero_list = [0 for x in self.a_value]
         self.assertEqual(self.a - self.a, et.Tensor(zero_list, self.a_indices))
+
+    def test_list_passed_to_tensor_as_only_input_raises_exception(self) -> None:
+        with self.assertRaises(ValueError):
+            b = self.a[[0]]
+
+    def test_list_passed_to_tensor_as_one_input_raises_exception(self) -> None:
+        rank_two_tensor = self.a * et.Tensor(self.a_value, ['^nu'])
+        with self.assertRaises(ValueError):
+            b = rank_two_tensor[[0],:]
+
+    def test_wrong_number_of_indices_passed_to_tensor_as_slice_raises_exception(self) -> None:
+        rank_two_tensor = self.a * et.Tensor(self.a_value, ['^nu'])
+        with self.assertRaises(ValueError):
+            b = rank_two_tensor[:]
+
+    def test_wrong_number_of_indices_passed_to_tensor_as_index_raises_exception(self) -> None:
+        rank_two_tensor = self.a * et.Tensor(self.a_value, ['^nu'])
+        with self.assertRaises(ValueError):
+            b = rank_two_tensor[0]
+
+    def test_rank_zero_tensor_equals_corresponding_scalar(self) -> None:
+        three = et.Tensor(3, [])
+        self.assertEqual(three, 3)
+
+    def test_one_index_and_one_slice_in_rank_two_tensor(self) -> None:
+        rank_two_tensor = self.a * et.Tensor(self.a_value, ['^nu'])
+        self.assertEqual(rank_two_tensor[:, 0], self.a)
 
 
 if __name__ == '__main__':
